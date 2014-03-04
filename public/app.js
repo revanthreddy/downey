6976373a036bce4f232c1d9badda3c6cb4b9743d
rev <rev@rev-VirtualBox.(none)>
2014-03-04 17:38:14 -0600

    rendering js pages

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var server  = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 server.listen(3000);
 //io.set( 'origins', '*niwsc.com*:*' );
@@ -14,3 +15,12 @@ app.use(express.static(__dirname + '/public'));
 app.use(app.router);
 
 
+io.sockets.on('connection', function(socket) {
+    console.log("initialising socketio ..");
+    
+});
+
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/cubeinitialize.html');
+});
