dfae150f67f2c2bfa03af21567f7e25c32ce3fa5
rev <rev@rev-VirtualBox.(none)>
2014-03-05 15:48:32 -0600

    renamed public to public_html

--- public_html/app.js
+++ public_html/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var server  = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var path = require('path');
+
 
 server.listen(3000);
 //io.set( 'origins', '*niwsc.com*:*' );
@@ -11,7 +13,8 @@ app.use(express.methodOverride());
 
 app.use(express.cookieParser());
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
 app.use(app.router);
 
 
@@ -23,4 +26,19 @@ io.sockets.on('connection', function(socket) {
 
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/cubeinitialize.html');
+});
+
+
+
+app.get('/rotate', function(req, res) {
+    io.sockets.emit("rotate", true);
+    
+});
+
+app.get('/move' , function(){
+    io.sockets.emit("move", true);
+});
+
+app.get('/scale' , function(){
+    io.sockets.emit("scale", true);
 });
