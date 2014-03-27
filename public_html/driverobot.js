a576d32afe92aa9d29989d2a3aceb50c9dca5cb3
rev <rev@rev-VirtualBox.(none)>
2014-03-27 09:16:16 -0500

    added code to send commands to robot

---
+++ public_html/driverobot.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app = express();
+var server  = require('http').createServer(app);
+var path = require('path');
+
+var currentCommand = 0;
+
+
+server.listen(3000);
+//io.set( 'origins', '*niwsc.com*:*' );
+//app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+app.use(express.cookieParser());
+
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(app.router);
+
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/command', function(req, res) {
+    var command = currentCommand;
+    currentCommand = 0;
+    return res.status(200).send(""+command);
+    
+});
+
+
+
+
+app.post('/left', function(req, res) {
+    currentCommand = 1;
+    res.sendfile(__dirname + '/index.html');
+});
+
+app.post('/right', function(req, res) {
+    currentCommand = 2;
+    res.sendfile(__dirname + '/index.html');
+});
+
+app.post('/back', function(req, res) {
+    currentCommand = 3;
+    res.sendfile(__dirname + '/index.html');
+});
+
+
+
+
+
