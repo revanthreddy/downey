74b1989a48a50beeb67f31287be64fcba47c385b
rev <rev@rev-VirtualBox.(none)>
2014-03-27 16:23:23 -0500

    robotapp

---
+++ public_html/robotapp.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var server  = require('http').createServer(app);
+var path = require('path');
+
+var command = '';
+
+
+server.listen(80);
+//io.set( 'origins', '*niwsc.com*:*' );
+//app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+app.use(express.cookieParser());
+
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(app.router);
+
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/command' , function(req,res){
+    return res.status(200).send(""+command);
+});
+
+app.put('/command/:next', function(req,res){
+    command = req.param('next');
+    return res.status(200).send("command set to "+command);
+});
