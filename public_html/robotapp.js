74ca4ec6787399175394d04c0ddb3b55485cb743
rev <rev@rev-VirtualBox.(none)>
2014-03-31 09:54:24 -0500

    adjusted robotapp

--- public_html/robotapp.js
+++ public_html/robotapp.js
@@ -28,7 +28,7 @@ app.get('/command' , function(req,res){
 });
 
 app.put('/command', function(req,res){
-    console.log(req.body);
-    command = req.body;
+    console.log(req.query.coords);
+    command = req.query.coords;
     return res.status(200).send("command set to "+command);
 });
