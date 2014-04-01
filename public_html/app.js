df9b8fd1330e7911093c1777a67f0aea7b6536f9
rev <rev@rev-VirtualBox.(none)>
2014-04-01 15:57:53 -0500

    added the support of beacons in app.js

--- public_html/app.js
+++ public_html/app.js
@@ -6,7 +6,12 @@ var path = require('path');
 
 var command = '';
 
-server.listen(80);
+var beacon1 = {"distance" : 10};
+var beacon2 = {"distance" : 10};
+var beacon3 = {"distance" : 10};
+var beacon4 = {"distance" : 10};
+
+server.listen(3000);
 //io.set( 'origins', '*niwsc.com*:*' );
 //app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -26,7 +31,7 @@ io.sockets.on('connection', function(socket) {
 
 
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/morecubeoperations.html');
+    res.sendfile(__dirname + '/cubephong.html');
 });
 
 
@@ -64,3 +69,40 @@ app.put('/command', function(req,res){
     return res.status(200).send("command set to "+command);
 });
 
+
+
+
+app.get('/beacon/:id', function(req,res){
+    var id = req.param("id");
+    if(id == 1)
+        return res.status(200).send(beacon1);
+    if(id == 2)
+        return res.status(200).send(beacon2);
+    if(id == 3)
+        return res.status(200).send(beacon3);
+    if(id == 4)
+        return res.status(200).send(beacon4);
+    return res.status(404).send("beacon not found");
+});
+
+app.put('/beacon/:id', function(req,res){
+    var id = req.param("id");
+    var distance = req.body.distance;
+    if(id == 1){
+        beacon1.distance = distance;
+        return res.status(200).send(beacon1);
+    }
+    if(id == 2){
+        beacon2.distance = distance;
+        return res.status(200).send(beacon2);
+    }
+    if(id == 3){
+        beacon3.distance = distance;
+        return res.status(200).send(beacon3);
+    }
+    if(id == 4){
+        beacon4.distance = distance;
+        return res.status(200).send(beacon4);
+    }
+    return res.status(404).send("beacon not found");
+});
